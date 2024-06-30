3c295b5c2090a02fd4e349475800edbf42088452
Victor RED <viktor_RED_rudko@mail.ru>
2024-06-30 10:58:35 +0300

    feat: solve hw1

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -4,39 +4,54 @@ import MessageSender from './message-sender/MessageSender'
 import s2 from '../../s1-main/App.module.css'
 import FriendMessage from './friend-message/FriendMessage'
 import avatar from './avatar.png'
+import avatar2 from './avatar2.png'
 
 /*
-* 1 - описать тип MessageType
-* 2 - описать тип MessagePropsType в файле Message.tsx
-* 3 - в файле Message.tsx отобразить приходящие данные
-* 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx
-* 5 - сделать стили в соответствии с дизайном
+* 1 - описать тип MessageType ++++
+* 2 - описать тип MessagePropsType в файле Message.tsx ++++
+* 3 - в файле Message.tsx отобразить приходящие данные ++++
+* 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx ++++
+* 5 - сделать стили в соответствии с дизайном ++++
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type MessageType = {
+  id: number
+  user: UserMessageType
+  message: MyMessageType
+};
+
+type UserMessageType = {
+  avatar: string
+  name: string
+}
+
+type MyMessageType = {
+  text: string
+  time: string
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
     id: 0,
     user: {
         avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
+        name: 'Viktor RED',  // можно менять
     },
     message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
-        time: '22:00', // можно менять
+        text: 'Hello my friend!!! How are you?!!!', // можно менять
+        time: '11:53', // можно менять
     },
 }
 export const friendMessage0: MessageType = {
     id: 100,
     user: {
-        avatar: avatar, // можно менять
-        name: 'Friend Name', // можно менять
+        avatar: avatar2, // можно менять
+        name: 'Den Liboorg', // можно менять
     },
     message: {
-        text: 'зеркальное сообщение для тренировки css', // можно менять
-        time: '22:00', // можно менять
+        text: "Oh, hi Victor! I'm fine!!! I continue to study at the IT-Incubator!!!", // можно менять
+        time: '12:09', // можно менять
     },
 }
 
