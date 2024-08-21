e2591fe6d8e8a9c2890da625139460a54fb8fbb8
Victor RED <viktor_RED_rudko@mail.ru>
2024-08-21 19:07:34 +0300

    feat: HW5

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -16,17 +16,23 @@ function Pages() {
         <div>
             {/*Routes выбирает первый подходящий роут*/}
             <Routes>
-                {/*роутинг будут писать студенты*/}
+              <Route path='/' element={<Navigate to={'/pre-junior'}/>}/>
+
+              <Route path='/pre-junior' element={<PreJunior/>}/>
+              <Route path='/junior' element={<Junior/>}/>
+              <Route path='/junior-plus' element={<JuniorPlus/>}/>
+              
+              <Route path='/*' element={<Error404/>}/>
+
+                {/* роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
                 {/*<Route ...*/}
-
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
                 {/*<Route ...*/}
                 {/*<Route ...*/}
                 {/*<Route ...*/}
-
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
+                {/*<Route ... */}
             </Routes>
         </div>
     )
