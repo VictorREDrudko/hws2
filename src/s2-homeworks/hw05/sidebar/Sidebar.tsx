7379c273477216cb934067a67639843f26de9ee7
Victor RED <viktor_RED_rudko@mail.ru>
2024-08-21 21:11:34 +0300

    feat: solve problems HW5 part 5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -17,7 +17,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
             {/*затемнение справа от открытого меню*/}
             {open && <div className={s.background} onClick={handleClose}/>}
 
-            <aside className={sidebarClass}>
+            {open && <aside className={sidebarClass}>
                 <button className={s.close} onClick={handleClose}>
                     <img
                         src={closeIcon}
@@ -31,7 +31,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        className={open ? s.active : ''}
+                        className={({isActive}) => isActive ? s.active : ''}
                         // className={...} // делает студент
                     >
                         Pre-junior
@@ -55,7 +55,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         Junior Plus
                     </NavLink>
                 </nav>
-            </aside>
+            </aside>}
         </>
     )
 }
