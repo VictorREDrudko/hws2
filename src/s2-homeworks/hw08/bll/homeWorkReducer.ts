67f3a0c84e3e42c91e19406a786b609701e4d5fc
Victor RED <viktor_RED_rudko@mail.ru>
2024-11-28 21:22:57 +0300

    feat: solve error HW7

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -3,7 +3,8 @@ import {UserType} from '../HW8'
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-            const sortState = state.sort((a: UserType, b: UserType) => {
+            const newState = [...state]
+            newState.sort((a: UserType, b: UserType) => {
               if (a.name < b.name) {
                 return -1;
               }
@@ -13,11 +14,7 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
               return 0;
             })
 
-            // const sortDownState = state.sort((a: UserType, b: UserType) => {
-            //   return a.name < b.name ? 1 : 0
-            // })
-
-            return action.payload === 'up' ? sortState : sortState.reverse()// need to fix
+            return action.payload === 'up' ? newState : newState.reverse()// need to fix
         }
         case 'check': {
             return state.filter(el => el.age >= action.payload)
